d81416cf444c9285fa6079b08a9d1b6af58b34b2
Robert Zacher <robert.zacher@one.verizon.com>
2016-08-30 18:04:31 -0400

    add post for todos

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var todos = [{
-	id: 1,
-	description: 'Meet mom for lunch',
-	completed: false
-}, {
-	id: 2,
-	description: 'go to market',
-	completed: false
-}, {
-	id: 3,
-	description: 'buy a lobster',
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
 	res.send('Todo API root'); 
@@ -47,7 +39,20 @@ app.get('/todos/:id', function (req, res) {
 		// send a 404, since we didn't return. 
 		res.status(404).send(); 
     }
+});
 
+// POST /todos
+app.post('/todos', function(req, res) {
+	var body = req.body;
+	
+	// add id field
+	body.id = todoNextId++;
+
+	console.log('got here');
+	// push body to array
+	todos.push(body);
+	console.log(body.description); 
+	res.json(body);
 });
 
 app.listen(PORT, function(){
