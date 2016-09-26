0f65162c5803a773cbe9d28507162d147dd7a0e1
Robert Zacher <robert.zacher@one.verizon.com>
2016-09-26 09:16:19 -0400

    Added completed filter

--- server.js
+++ server.js
@@ -10,14 +10,27 @@ var todoNextId = 1;
 
 app.use(bodyParser.json());
 
+// Get /
 app.get('/', function(req, res) {
+ 
 	res.send('Todo API root'); 
 });
 
 
-// GET /todos
+// GET /todos?completed = true
 app.get('/todos', function (req, res) {
-	res.json(todos);
+	var queryParams = req.query; 
+	filteredTodos = todos;
+
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	}
+	else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+	
+
+	res.json(filteredTodos);
 });
 // GET /todos/:id
 app.get('/todos/:id', function (req, res) {
