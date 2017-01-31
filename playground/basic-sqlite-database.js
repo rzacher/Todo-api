3e38b2e95607313b2a82b0171c6a1c8403e5d6c2
Robert Zacher <robert.zacher@one.verizon.com>
2017-01-31 14:56:31 -0500

    added some sequelize functionality

---
+++ playground/basic-sqlite-database.js
@@ -0,0 +1,31 @@
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize(undefined, undefined, undefined, 
+	{'dialect':'sqlite',
+     'storage': __dirname + '/basic-sqlite-database.sqlite'
+ });
+
+var Todo = sequelize.define('todo', {
+	description: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			len: [1, 250]
+		}
+	}, completed: {
+		type: Sequelize.BOOLEAN,
+		allowNull: false,
+		defaultValue: false
+	}
+})
+
+sequelize.sync().then(function () {
+	console.log('Everything is synced'); 
+	Todo.create({description: 'Walking my dog',
+                 completed:false
+                }).then(function (todo) {
+                   console.log('Finished!');
+                   console.log(todo); 
+                }).catch(function (e) {
+                	console.log(e); 
+                })
+})
