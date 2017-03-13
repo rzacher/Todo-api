f36d8f5d83bf85a29e89e15b756690186e7db72f
Robert Zacher <robert.zacher@one.verizon.com>
2017-03-13 16:40:47 -0400

    Convert todos to use sequelize

--- server.js
+++ server.js
@@ -77,21 +77,22 @@ app.post('/todos', function(req, res) {
 // DELETE /todos/:id
 app.delete('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
-   
 
-    var matchedTodo = _.findWhere(todos, {id: todoId});
-
-    console.log('in delete'); 
-    if (matchedTodo) {
-    	console.log("Deleting /todos " + matchedTodo); 
-		var newTodos = _.without(todos, matchedTodo);
-		todos = newTodos; 
-    	res.json(matchedTodo);
-    	return; 
-    } else {
-		// send a 404, since we didn't return. 
-		res.status(404).send(); 
-    }
+	db.todo.destroy({
+		where: {
+			id:todoId
+		}
+	}).then(function (rowsDeleted) {
+		if (rowsDeleted === 0) {
+			res.status(404).json({
+				error: 'No todo with id'
+			});
+		} else {
+			res.status(204).send(); 
+		}
+	}, function (e) {
+		res.status(500).send();
+	}); 
 });
 
 // PUT  /todos/:id
