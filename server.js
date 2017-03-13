1a298aa1b6d1b0bd525b23b13a0adeff66857dcf
Robert Zacher <robert.zacher@one.verizon.com>
2017-03-13 10:45:09 -0400

    Added query to get

--- server.js
+++ server.js
@@ -20,24 +20,44 @@ app.get('/', function(req, res) {
 
 // GET /todos?completed = true&q=house
 app.get('/todos', function (req, res) {
-	var queryParams = req.query; 
-	filteredTodos = todos;
+	var query = req.query; 
+	var where = {};
 
-	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-		filteredTodos = _.where(filteredTodos, {completed: true});
+	if (query.hasOwnProperty('completed') && query.completed === 'true') {
+		where.completed = true;
+	} else if (query.hasOwnProperty('completed') && query.completed === 'false') {
+		where.completed = false; 
 	}
-	else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-		filteredTodos = _.where(filteredTodos, {completed: false});
-	}
-	
-    // .indexOf(q) > -1
-    if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-    	filteredTodos = _.filter(filteredTodos, function (todo) {
-    		return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1; 
-        }); 
+
+    if (query.hasOwnProperty('q') && query.q.length > 0) {
+    	where.description = {
+          $like: '%' + query.q + '%'
+        };
     }
 
-	res.json(filteredTodos);
+	db.todo.findAll({where: where}).then(function (todos) {
+           res.json(todos);
+       }, function (e) {
+         res.status(500).send(); 
+     });
+
+	// filteredTodos = todos;
+
+	// if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+	// 	filteredTodos = _.where(filteredTodos, {completed: true});
+	// }
+	// else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+	// 	filteredTodos = _.where(filteredTodos, {completed: false});
+	// }
+	
+ //    // .indexOf(q) > -1
+ //    if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+ //    	filteredTodos = _.filter(filteredTodos, function (todo) {
+ //    		return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1; 
+ //        }); 
+ //    }
+
+	//res.json(filteredTodos);
 });
 // GET /todos/:id
 app.get('/todos/:id', function (req, res) {
