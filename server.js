f5929f958cd3b75415a85722cd88e4471e231401
Robert Zacher <robert.zacher@one.verizon.com>
2017-03-16 13:32:48 -0400

    hash and salt password

--- server.js
+++ server.js
@@ -136,7 +136,7 @@ app.post('/users', function(req, res) {
 	console.log(body); 	
 
     db.user.create(body).then(function (user) {
-               res.json(user.toJSON());
+               res.json(user.toPublicJSON());
             }, function (e) {
               res.status(400).json(e);
               console.log(e); 
